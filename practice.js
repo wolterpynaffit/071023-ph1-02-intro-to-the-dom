800b432fc6cf548477c680a6cb8edb73d078fe97
brewchetta <chett.tiller@gmail.com>
2023-07-11 12:57:28 -0400

    added practice, lecture comments

---
+++ practice.js
@@ -0,0 +1,41 @@
+// // PAIR PROGRAMMING PRACTICE // //
+// wait until groups have been assigned before you attempt these challenges!
+// fork and clone this repo and follow the instructions in this file
+
+
+// // GETTING STARTED // //
+// To get started, add this line of code to the index.html somewhere in the <head> tag:
+// <script defer src="practice.js" charset="utf-8"></script>
+// this will allow you to write in this file and see changes on the DOM
+
+// create a new div with an id of .movie-posters and append it to the body
+
+// do this three times:
+
+// find a movie poster you like and add it as an <img> inside the .movie-poster div
+
+// beneath each <img> add the movie title as an <h2>
+
+// further refine the movie posters:
+// each movie poster gets its own <div> which includes the poster <img>, the title <h2>
+// and a short description <p>
+
+// once you have the movie posters can you...
+
+// console.log the title and description for the first movie poster?
+
+// change the <img> src, title text, and description text for the second movie poster?
+
+// delete the last movie poster?
+
+
+
+// // HINTS // //
+
+// given that adding movie posters is a repetetive task, it may be easier to build
+// a function that takes in certain arguments and does the work for you...
+
+// remember that you need a few steps in order to finally see a dom element on the page:
+// create the element
+// change attributes like text or src
+// append the element
